e975bd7a4ddfa72ad94e20583e947ad5b63ed965
Gordon Marler <gmarler@gmarler.com>
2016-03-29 11:52:04 -0400

    First checkins

--- lib/Tree/AA.pm
+++ lib/Tree/AA.pm
@@ -2,6 +2,24 @@ package Tree::AA;
 
 use strict;
 use warnings;
+use v5.20;
+
+use Moose;
+
+has 'node' => ( is => 'rw', isa => 'Any' );
+
+has 'left' => (
+  is        => 'rw',
+  isa       => 'Tree::AA',
+  predicate => 'has_left',
+  lazy      => 1,
+  default   => sub { Tree::AA->new( $_[0] ) },
+  trigger   => \&set_something,
+);
+
+has 'right' => ( );
+
+has 'level' => ( is => 'rw' );
 
 
 
