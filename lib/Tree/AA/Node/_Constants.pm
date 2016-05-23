ba8d2cf87d5ee9bbc5977424fb498c4b7442d330
gmarler <gmarler@gmarler.com>
2016-05-22 20:27:36 -0400

    Properly export constants

--- lib/Tree/AA/Node/_Constants.pm
+++ lib/Tree/AA/Node/_Constants.pm
@@ -3,14 +3,20 @@ use warnings;
 package Tree::AA::Node::_Constants;
 
 use Carp;
+use vars qw( @EXPORT );
 
 # VERSION
+
+require Exporter;
+*import = \&Exporter::import;
 
 my @Node_slots;
 
 BEGIN {
   @Node_slots = qw( LEFT RIGHT LEVEL KEY VAL );
 }
+
+@EXPORT = (map {"_$_"} @Node_slots);
 
 use enum @Node_slots;
 
